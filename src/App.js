f35f69da129c73eeaa6de1fd35f7bb99a5779073
rahul-rdb <rahulbobade26@gmail.com>
2022-11-11 10:35:12 +0530

    hompage changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   
   return (
     <div className="App">
-      <Header />
+      <Header path="/"/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route exact path="/coins/:id" element={<CoinsPage/>}/>
